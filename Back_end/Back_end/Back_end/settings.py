4e9153769ea6853b96cc86949f5b08ea2e7442e3
Muppudathi076 <varathamathavan@gmail.com>
2026-05-02 17:53:18 +0530

    cors update

--- Back_end/Back_end/Back_end/settings.py
+++ Back_end/Back_end/Back_end/settings.py
@@ -26,16 +26,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h^jizalyheof@*t$0^0dwz_d0k!al#vs9xy#dl5xv=d&n@vl@)'
-
+# SECRET_KEY = 'django-insecure-h^jizalyheof@*t$0^0dwz_d0k!al#vs9xy#dl5xv=d&n@vl@)'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
 # ALLOWED_HOSTS = ['sqlmaster.onrender.com', 'localhost', '127.0.0.1', '*']
-DEBUG = False
-
-ALLOWED_HOSTS = ['*']
-
+# DEBUG = False
+DEBUG = os.environ.get("DEBUG") == "True"
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 # Application definition
 
 INSTALLED_APPS = [
@@ -167,10 +167,11 @@ SIMPLE_JWT = {
 }
 
 # CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS')
-CORS_ALLOW_ALL_ORIGINS = True
-CSRF_TRUSTED_ORIGINS = [
-    # "http://localhost:3000",
-    "https://sql-master-rwon.vercel.app"
-]
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS").split(",")
+# CSRF_TRUSTED_ORIGINS = [
+#     # "http://localhost:3000",
+#     "https://sql-master-rwon.vercel.app"
+# ]
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(",")
 CORS_ALLOW_ALL_HEADERS = True
 CORS_ALLOW_CREDENTIALS = True
